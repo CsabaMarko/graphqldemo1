c8baf738501a77a322790a7a6299c04a503e321f
CsabaMarko <marko.csaba@gmail.com>
2022-10-25 00:58:52 +0200

    Added and mapped ChangeRequest, changed User; added relevant schema and data SQL pieces

---
+++ src/main/java/com/csabamarko/springboot/jpa/graphqldemo1/snow/ChangeRequest.java
@@ -0,0 +1,69 @@
+package com.csabamarko.springboot.jpa.graphqldemo1.snow;
+
+import com.csabamarko.springboot.jpa.graphqldemo1.RootEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.OffsetDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "CHANGE_REQUEST", schema = "SNOW")
+public class ChangeRequest extends RootEntity<String> {
+
+    @Id
+    @Column(name = "ID")
+    private String id;
+
+    @Column(name = "CHANGE_NUMBER")
+    private String changeNumber;
+
+    @Column(name = "SHORT_DESCRIPTION")
+    private String shortDescription;
+
+    @Column(name = "DESCRIPTION")
+    private String description;
+
+    @Column(name = "ACTIVE_INDICATOR")
+    private String active;
+
+    @Column(name = "CHANGE_REQUEST_TYPE")
+    private String type;
+
+    @Column(name = "WORK_START_DATE")
+    private OffsetDateTime workStart;
+
+    @Column(name = "WORK_END_DATE")
+    private OffsetDateTime workEnd;
+
+    @Column(name = "TIME_WORKED_HOURS")
+    private Integer timeWorkedHours;
+
+    @Column(name = "CREATED_TIMESTAMP")
+    private OffsetDateTime createdAt;
+
+    @Column(name = "UPDATED_TIMESTAMP")
+    private OffsetDateTime updatedAt;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ASSIGNED_TO_USER_ID")
+    private User assignedTo;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "CREATED_BY_USER_ID")
+    private User createdBy;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "REQUESTED_BY_USER_ID")
+    private User requestedBy;
+
+    @ManyToMany()
+    @JoinTable(name = "CHANGE_REQUEST_X_AFFECTED_USERS",
+        joinColumns = @JoinColumn(name = "CHANGE_REQUEST_ID"), inverseJoinColumns = @JoinColumn(name = "USERS_ID")
+    )
+    private List<User> affectedUsers;
+
+}
--- src/main/java/com/csabamarko/springboot/jpa/graphqldemo1/snow/User.java
+++ src/main/java/com/csabamarko/springboot/jpa/graphqldemo1/snow/User.java
@@ -3,26 +3,41 @@ package com.csabamarko.springboot.jpa.graphqldemo1.snow;
 import com.csabamarko.springboot.jpa.graphqldemo1.RootEntity;
 import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "USERX", schema = "SNOW") // H2 doesn't allow "USER"; probably reserved word?
+@Table(name = "USERS", schema = "SNOW") // H2 doesn't allow "USER"; probably reserved word?
 public class User extends RootEntity<String> {
 
     @Id
     @Column(name = "ID")
     private String id;
 
+    @Column(name = "SN_ID")
+    private String snId;
+
     @Column(name = "FIRST_NAME")
     private String firstName;
 
     @Column(name = "LAST_NAME")
     private String lastName;
+
+    @Column(name = "MIDDLE_NAME")
+    private String middleName;
+
+    @Column(name = "PHONE_NUMBER")
+    private String phone;
+
+    @Column(name = "EMAIL_ADDRESS")
+    private String email;
+
+    // This the "inverse" mapping. The "forward" is defined on ChangeRequest.affectedUsers:
+    @ManyToMany(mappedBy = "affectedUsers")
+    private List<ChangeRequest> affectedByChangeRequests;
+
 }
